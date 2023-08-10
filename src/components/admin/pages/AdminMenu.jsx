e84e333135a8809564429e678a76b845f3b47a15
Harsh Patel <harsh3608@gmail.com>
2023-08-10 12:08:18 +0530

    implemented question listing and css , created helpers file

--- src/components/admin/pages/AdminMenu.jsx
+++ src/components/admin/pages/AdminMenu.jsx
@@ -1,72 +1,55 @@
 import React from "react";
-// import { BrowserRouter, Routes, Route, } from "react-router-dom";
-// import ProtectedRoute from "../../../util/ProtectedRoute";
-// import AddUser from "../../../common/AddUser";
-// import QuestionList from "./questions/QuestionList";
-// import ExamList from "./exams/ExamList";
-// import ResultList from "./results/ResultList";
+import "../../admin/styles/AdminMenu.css"
 
 const AdminMenu = () => (
     <div>
-        <nav className="navbar navbar-expand-lg  bg-light">
-            <img
-                alt="logo"
-                src="/images/convergesol-final-logo.png"
-                width="100"
-                height="40"
-                className="d-inline-block align-top"
-            />
-            <h3 className="navbar-brand" >
-                Online Exam
-            </h3>
-            <button
-                className="navbar-toggler"
-                type="button"
-                data-toggle="collapse"
-                data-target="#navbarNav"
-                aria-controls="navbarNav"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-            >
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
+        <nav className="navbar navbar-expand-lg d-flex flex-row justify-content-between">
+            <div className="brand mx-3">
+                <img
+                    alt="logo"
+                    src="/images/convergesol-final-logo.png"
+                    width="100"
+                    height="40"
+                    className="d-inline-block align-top"
+                />
+                &nbsp;&nbsp;
+                <h2 className="navbar-brand mt-2" >
+                    Online Exam
+                </h2>
+            </div>
+
+
+
+
+
+            <div className="collapse navbar-collapse d-flex justify-content-end mx-3" id="navbarNav" >
                 <ul className="navbar-nav ml-auto">
                     <li className="nav-item">
-                        <a className="nav-link" href="/admin/add-candidate">
+                        <a className="nav-link text-dark" href="/admin/add-candidate" >
                             Add Candidate
                         </a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="/admin/questions">
+                        <a className="nav-link text-dark" href="/admin/questions">
                             Questions
                         </a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="/admin/exams">
+                        <a className="nav-link text-dark" href="/admin/exams">
                             Exams
                         </a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="/admin/results">
+                        <a className="nav-link  text-dark" href="/admin/results">
                             Results
                         </a>
+                    </li>
+                    <li className="nav-item">
+                        <button className="btn btn-light"><span className="text-danger">LogOut</span></button>
                     </li>
                 </ul>
             </div>
         </nav>
-
-
-        {/* <div>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/admin/add-candidate" element={<ProtectedRoute><AddUser /></ProtectedRoute>} />
-                    <Route path="/admin/questions" element={<ProtectedRoute><QuestionList /></ProtectedRoute>} />
-                    <Route path="/admin/exams" element={<ProtectedRoute><ExamList /></ProtectedRoute>} />
-                    <Route path="/admin/results" element={<ProtectedRoute><ResultList /></ProtectedRoute>} />
-                </Routes>
-            </BrowserRouter>
-        </div> */}
 
 
     </div>
--- src/components/admin/pages/questions/QuestionList.jsx
+++ src/components/admin/pages/questions/QuestionList.jsx
@@ -1,11 +1,107 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import AdminMenu from "../AdminMenu";
+import "../../styles/QuestionList.css";
 
 const QuestionList = () => {
+    const [data, setData] = useState([]);
+    const [sortColumn, setSortColumn] = useState(null);
+    const [sortDirection, setSortDirection] = useState('asc');
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        try {
+            const token = localStorage.getItem('token');
+
+            const response = await fetch('http://13.90.224.87:8099/api/Questions/GetAllAsync?pageIndex=1&pageSize=500', {
+                method: "GET",
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`,
+                },
+
+            });
+            const json = await response.json();
+            if (json.isSuccess) {
+                setData(json.response);
+            }
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
+    const handleSort = (column) => {
+        if (sortColumn === column) {
+            setSortDirection(sortDirection === 'asc' ? 'desc' : 'asc');
+        } else {
+            setSortColumn(column);
+            setSortDirection('asc');
+        }
+    };
+
+    const sortedData = data.slice().sort((a, b) => {
+        const aValue = a[sortColumn];
+        const bValue = b[sortColumn];
+        return sortDirection === 'asc' ? aValue - bValue : bValue - aValue;
+    });
+
     return (
         <>
-        <AdminMenu />
-        <h2>Questions List</h2>
+            <AdminMenu />
+            <div className="px-2">
+
+                <div className="d-flex flex-row justify-content-between">
+                    <h1>Manage Questions</h1>
+
+                    <div className="mt-2">
+                        <button className="btn btn-primary">
+                            <div className="d-flex flex-row">
+                                <span class="material-icons">
+                                    add
+                                </span>
+                                <span>Add</span>
+                            </div>
+                        </button>
+                    </div>
+
+                </div>
+
+
+                <hr></hr>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th onClick={() => handleSort('id')}>Technology</th>
+                            <th onClick={() => handleSort('questionName')}>Question</th>
+                            <th onClick={() => handleSort('marks')}>Experience Level</th>
+                            <th onClick={() => handleSort('timeSlot')}>Active</th>
+                            <th>Created On</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {sortedData.map((question) => (
+                            <tr key={question.id}>
+                                <td style={{ 'width': '200px' }}>{question.technology}</td>
+                                <td className="ellipsis-200" >{question.questionName}</td>
+                                <td>{question.experienceLevel}</td>
+                                <td>{question.active}</td>
+                                <td>{question.createdOn}</td>
+                                <td>
+                                    <button className="btn btn-warning">
+                                        <span class="material-icons">
+                                            edit_square
+                                        </span>
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </>
     );
 };
