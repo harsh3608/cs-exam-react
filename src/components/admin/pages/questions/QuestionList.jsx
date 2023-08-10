b796ee7c458c61c9e55750f40f3263b1dfb9fa4f
Harsh Patel <harsh3608@gmail.com>
2023-08-10 17:33:21 +0530

    added loaders in ques list

--- src/components/admin/pages/questions/QuestionList.jsx
+++ src/components/admin/pages/questions/QuestionList.jsx
@@ -9,9 +9,15 @@ const QuestionList = () => {
     const [sortDirection, setSortDirection] = useState('asc');
     const [currentPage, setCurrentPage] = useState(1);
     const [rowsPerPage, setRowsPerPage] = useState(10);
+    const [loading, setLoading] = useState(true);
+
 
     useEffect(() => {
-        fetchData();
+        setTimeout(() => {
+            fetchData();
+            setLoading(false);
+        }, 3000);
+        
     }, [currentPage, rowsPerPage]);
 
     const fetchData = async () => {
@@ -46,11 +52,22 @@ const QuestionList = () => {
         }
     };
 
-    const sortedData = data.slice().sort((a, b) => {
+    const compareValues = (a, b) => {
         const aValue = a[sortColumn];
         const bValue = b[sortColumn];
-        return sortDirection === 'asc' ? aValue - bValue : bValue - aValue;
-    });
+
+        if (typeof aValue === 'string' || aValue instanceof String) {
+            return sortDirection === 'asc' ? aValue.localeCompare(bValue) : bValue.localeCompare(aValue);
+        } else if (typeof aValue === 'number') {
+            return sortDirection === 'asc' ? aValue - bValue : bValue - aValue;
+        } else if (aValue instanceof Date) {
+            return sortDirection === 'asc' ? aValue - bValue : bValue - aValue;
+        } else {
+            return 0; // Default case: no sorting
+        }
+    };
+
+    const sortedData = data.slice().sort(compareValues);
 
     const lastIndex = currentPage * rowsPerPage;
     const firstIndex = lastIndex - rowsPerPage;
@@ -59,114 +76,157 @@ const QuestionList = () => {
     return (
         <>
             <AdminMenu />
-            <div className="px-2">
-                <div className="d-flex flex-row justify-content-between">
-                    <h1>Manage Questions</h1>
-                    <div className="mt-2">
-                        <button className="btn btn-primary">
-                            <div className="d-flex flex-row">
-                                <span className="material-icons">
-                                    add
-                                </span>
-                                <span>Add</span>
-                            </div>
-                        </button>
-                    </div>
-                </div>
-                <hr></hr>
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <th >
-                                Technology
-                                <button onClick={() => handleSort('technology')}>
-                                    <span className="material-icons vertical-icon">
-                                        {sortColumn === 'technology' ? (sortDirection === 'asc' ? 'arrow_upward' : 'arrow_downward') : 'sync_alt'}
-                                    </span>
-                                </button>
+            {loading ? (
+                <div  style={{'marginTop':'20%', 'marginLeft':'45%'}}>
+                    {/* <div className="spinner-border text-info" style={{'scale':'2.0'}}></div> */}
+                
+                    <div className="lds-facebook"><div></div><div></div><div></div></div>
 
-                            </th>
-                            <th onClick={() => handleSort('questionName')}>Question</th>
-                            <th onClick={() => handleSort('experienceLevel')}>Experience Level</th>
-                            <th onClick={() => handleSort('active')}>Active</th>
-                            <th onClick={() => handleSort('createdOn')}>Created On</th>
-                            <th>Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {currentData.map((question) => (
-                            <tr key={question.id}>
-                                <td style={{ width: '200px' }}>{question.technology}</td>
-                                <td className="ellipsis-300">{question.questionName}</td>
-                                <td>{question.experienceLevel}</td>
-                                <td>{question.active}</td>
-                                <td>{FormatDate(question.createdOn)}</td>
-                                <td>
-                                    <button className="btn btn-warning">
-                                        <span className="material-icons">
-                                            edit_square
+                </div>
+            ) : (
+                <div className="px-2">
+                    <div className="d-flex flex-row justify-content-between">
+                        <h1>Manage Questions</h1>
+                        <div className="mt-2">
+                            <button className="btn btn-primary">
+                                <div className="d-flex flex-row">
+                                    <span className="material-icons">
+                                        add
+                                    </span>
+                                    <span>Add</span>
+                                </div>
+                            </button>
+                        </div>
+                    </div>
+                    <hr></hr>
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th >
+                                    Technology
+                                    &nbsp;
+                                    <button className="btn btn-light" onClick={() => handleSort('technology')}>
+                                        <span className="material-icons vertical-icon">
+                                            {sortColumn === 'technology' ? (sortDirection === 'asc' ? 'keyboard_double_arrow_up' : 'keyboard_double_arrow_down') : 'filter_list'}
                                         </span>
                                     </button>
-                                </td>
+
+                                </th>
+                                <th>
+                                    Question
+                                    &nbsp;
+                                    <button className="btn btn-light" onClick={() => handleSort('questionName')}>
+                                        <span className="material-icons vertical-icon">
+                                            {sortColumn === 'questionName' ? (sortDirection === 'asc' ? 'keyboard_double_arrow_up' : 'keyboard_double_arrow_down') : 'filter_list'}
+                                        </span>
+                                    </button>
+                                </th>
+                                <th>
+                                    Experience Level
+                                    &nbsp;
+                                    <button className="btn btn-light" onClick={() => handleSort('experienceLevel')}>
+                                        <span className="material-icons vertical-icon">
+                                            {sortColumn === 'experienceLevel' ? (sortDirection === 'asc' ? 'keyboard_double_arrow_up' : 'keyboard_double_arrow_down') : 'filter_list'}
+                                        </span>
+                                    </button>
+                                </th>
+                                <th>
+                                    Active
+                                    &nbsp;
+                                    <button className="btn btn-light" onClick={() => handleSort('active')}>
+                                        <span className="material-icons vertical-icon">
+                                            {sortColumn === 'active' ? (sortDirection === 'asc' ? 'keyboard_double_arrow_up' : 'keyboard_double_arrow_down') : 'filter_list'}
+                                        </span>
+                                    </button>
+                                </th>
+                                <th>
+                                    Created On
+                                    &nbsp;
+                                    <button className="btn btn-light" onClick={() => handleSort('createdOn')}>
+                                        <span className="material-icons vertical-icon">
+                                            {sortColumn === 'createdOn' ? (sortDirection === 'asc' ? 'keyboard_double_arrow_up' : 'keyboard_double_arrow_down') : 'filter_list'}
+                                        </span>
+                                    </button>
+                                </th>
+                                <th>Actions</th>
                             </tr>
-                        ))}
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            {currentData.map((question) => (
+                                <tr key={question.id}>
+                                    <td style={{ width: '200px' }}>{question.technology}</td>
+                                    <td className="ellipsis-300">{question.questionName}</td>
+                                    <td>{question.experienceLevel}</td>
+                                    <td>{question.active}</td>
+                                    <td>{FormatDate(question.createdOn)}</td>
+                                    <td>
+                                        <button className="btn btn-warning">
+                                            <span className="material-icons">
+                                                edit_square
+                                            </span>
+                                        </button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
 
 
-                <div className="d-flex flex-row justify-content-around m-4">
+                    <div className="d-flex flex-row justify-content-around m-4">
 
-                    <div className="entries-count">
-                        <h5 className="mt-2">
-                            Showing {Math.min(firstIndex + 1, sortedData.length)} to {Math.min(lastIndex, sortedData.length)} of {sortedData.length} entries
-                        </h5>
-                    </div>
+                        <div className="entries-count">
+                            <h5 className="mt-2">
+                                Showing {Math.min(firstIndex + 1, sortedData.length)} to {Math.min(lastIndex, sortedData.length)} of {sortedData.length} entries
+                            </h5>
+                        </div>
 
-                    <div className="pagination">
-                        <button className="btn btn-link"
-                            onClick={() => setCurrentPage(currentPage - 1)}
-                            disabled={currentPage === 1} >
-                            <div className="d-flex">
-                                <span class="material-icons">
-                                    keyboard_double_arrow_left
-                                </span>
-                            </div>
-                        </button>
+                        <div className="pagination">
+                            <button className="btn btn-link"
+                                onClick={() => setCurrentPage(currentPage - 1)}
+                                disabled={currentPage === 1} >
+                                <div className="d-flex">
+                                    <span className="material-icons">
+                                        keyboard_double_arrow_left
+                                    </span>
+                                </div>
+                            </button>
 
-                        &nbsp;&nbsp;
+                            &nbsp;&nbsp;
 
-                        <h5 className="mt-2">Page {currentPage}</h5>
+                            <h5 className="mt-2">Page {currentPage}</h5>
 
-                        &nbsp;&nbsp;
+                            &nbsp;&nbsp;
 
-                        <button
-                            className="btn btn-link"
-                            onClick={() => setCurrentPage(currentPage + 1)}
-                            disabled={lastIndex >= sortedData.length}>
-                            <div className="d-flex">
-                                <span class="material-icons">
-                                    keyboard_double_arrow_right
-                                </span></div>
-                        </button>
+                            <button
+                                className="btn btn-link"
+                                onClick={() => setCurrentPage(currentPage + 1)}
+                                disabled={lastIndex >= sortedData.length}>
+                                <div className="d-flex">
+                                    <span className="material-icons">
+                                        keyboard_double_arrow_right
+                                    </span></div>
+                            </button>
 
-                    </div>
+                        </div>
 
-                    <div className="rows-per-page d-flex">
-                        <h5 className="mt-2">Rows per page:</h5>
-                        &nbsp;
-                        <select
-                            value={rowsPerPage}
-                            onChange={(e) => setRowsPerPage(parseInt(e.target.value))}
-                        >
-                            <option value={10}>10</option>
-                            <option value={20}>20</option>
-                            <option value={50}>50</option>
-                        </select>
+                        <div className="rows-per-page d-flex">
+                            <h5 className="mt-2">Rows per page:</h5>
+                            &nbsp;
+                            <select
+                                value={rowsPerPage}
+                                onChange={(e) => setRowsPerPage(parseInt(e.target.value))}
+                            >
+                                <option value={10}>10</option>
+                                <option value={20}>20</option>
+                                <option value={50}>50</option>
+                            </select>
+                        </div>
+
                     </div>
 
                 </div>
+            )}
 
-            </div>
         </>
     );
 };
--- src/util/Helpers.jsx
+++ src/util/Helpers.jsx
@@ -4,3 +4,4 @@ export const FormatDate = (dateString) => {
     const options = { day: 'numeric', month: 'short', year: 'numeric' };
     return new Date(dateString).toLocaleDateString('en-US', options);
 };
+
